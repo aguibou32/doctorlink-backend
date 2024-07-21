d2e982b851787cf064fe36b7618c9319121744fe
Aguibou Barry <aguibou32@gmail.com>
2024-07-21 15:28:56 -0400

    Remove node_modules from repository

---
+++ models/UserModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose"
+import bcrypt from 'bcrypt'
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  role: { type: String, enum: ['patient', 'doctor', 'admin'], default: 'patient' },
+},
+  { timestamps: true }
+)
+
+userSchema.methods.matchPassword = async function (enterredPassword){
+  return await bcrypt.compare(enterredPassword, this.password)
+}
+
+const User = mongoose.model("User", userSchema)
+export default User
---
+++ server.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import connectDB from "./config/db.js"
+import express from 'express'
+
+console.log(`Mongo URI: ${process.env.MONGO_URI}`); // Log the URI to debug
+
+connectDB()
+
+const app = express()
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server Running on port: ${port}`.green.inverse))
